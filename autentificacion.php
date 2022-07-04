251417fb6d315311da4187298c2a4a92ec3bf528
FarlenUrena <farlen.urena.naranjo@est.una.ac.cr>
2022-07-03 21:31:45 -0600

    sixth commit

--- autentificacion.php
+++ autentificacion.php
@@ -8,7 +8,15 @@ $usuario = $url["root"];
 $clave = $url[""];
 $base_datos = substr($url["bd_redsocial"],1);
 
-$conexion = new Conexion($servidor,$usuario,$clave,$base_datos);
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+
+$conexion = new Conexion($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
             // $this->servidor = $url[$servidor];
             // $this->usuario = $url[$usuario];
